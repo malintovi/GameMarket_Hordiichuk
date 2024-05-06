43abfece04fa755d08b312fc77ccaa5afec44f01
malintovi <bohdanhordiichuk1@gmail.com>
2024-05-06 13:02:52 +0300

    werqe

---
+++ market.js
@@ -0,0 +1,22 @@
+let itemsDiv = document.getElementById("items");
+console.log(itemsDiv);
+if(itemsDiv){
+    console.log(itemsDiv);
+    //console.log('Поле classList: ', itemsDiv.classList)
+    //console.log('Поле id: ', itemsDiv.id)
+    //console.log('Поле clientWidth: ', itemsDiv.classWidth)
+    //console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+
+    //itemsDiv.innerText = 'повартсмщдшр'
+    //itemsDiv.innerHTML = '<h1>acoustic</h1>'
+    //itemsDiv.innerHTML = '<div class="item></div>"'
+    //itemsDiv.innerHTML = '<div class="item></div>"'
+    //itemsDiv.innerHTML = '<div class="item></div>"'
+    //itemsDiv.innerHTML = '<div class="item></div>"'
+    // for(let i=0;i<100;i++){
+    //    itemsDiv.innerHTML += '<div class="item></div>"'
+    // }
+}
+else{
+    console.log('Блок товарів не знайдено')
+}
