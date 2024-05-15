d635f98b76b3f461a48d32ebe6d0100b93168bb4
malintovi <bohdanhordiichuk1@gmail.com>
2024-05-15 12:26:02 +0300

    ваі

--- market.js
+++ market.js
@@ -1,21 +1,16 @@
-/* let itemsDiv = document.getElementById("items");
-console.log(itemsDiv);
+ let itemsDiv = document.getElementById("items");
+ let itemArrey = [
+    'Венцель',
+    'Шатківський',
+    'Кучер'
+
+ ];
 if(itemsDiv){
-    console.log(itemsDiv);
-    console.log('Поле classList: ', itemsDiv.classList)
-    console.log('Поле id: ', itemsDiv.id)
-    console.log('Поле clientWidth: ', itemsDiv.classWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-    itemsDiv.innerText = 'повартсмщдшр'
-    itemsDiv.innerHTML = '<h1>acoustic</h1>'
-    itemsDiv.innerHTML = '<div class="item></div>"'
-    itemsDiv.innerHTML = '<div class="item></div>"'
-    itemsDiv.innerHTML = '<div class="item></div>"'
-    itemsDiv.innerHTML = '<div class="item></div>"'
-     for(let i=0;i<100;i++){
-        itemsDiv.innerHTML += '<div class="item"></div>'
-    }
+    itemArrey.forEach((item,index) =>{
+        itemsDiv.innerHTML +=
+
+        <div class = "items">
+            <h2>Вчитель № ${index + 1} з ${itemArrey.length}</h2>
+        </div>
+    })
 }
-else{
-    console.log('Блок товарів не знайдено')
-} */
