a8332b7cedf87c93e104c8777aac65e56402e8d5
malintovi <bohdanhordiichuk1@gmail.com>
2024-05-19 13:43:18 +0300

    dfg

--- market.js
+++ market.js
@@ -1,16 +1,55 @@
- let itemsDiv = document.getElementById("items");
- let itemArrey = [
-    'Венцель',
-    'Шатківський',
-    'Кучер'
+let itemsArray = [
+    {
+firstName: "Biіталій",
+lastName: "Шатківський",
+age: 43,
+subject: "CS",
+photo:"https://lyceum.ztu.rdu.ua/wp-content/uploads/2823/03/dsc07815-683x1024.jpg",
+ url: "https://lyceum.ztu.edu.us/team/shatkivskyy-v-m/",
+},
+{
+firstName: "Наталія",
+lastName: "Вeнце",
+age: 18,
+subject: "Адміністратор",
+photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2025/04/ventsel-nataliva-vasylivna-683x1024.jpg",
+ url: "https://lyceum.ztu.edu.ua/team/ventsel niv/",
 
- ];
-if(itemsDiv){
-    itemArrey.forEach((item,index) =>{
-        itemsDiv.innerHTML +=
-
-        <div class = "items">
-            <h2>Вчитель № ${index + 1} з ${itemArrey.length}</h2>
+},
+{
+firstName: "Наталія",
+lastName: "Кучер",
+age: 18,
+subject: "Фізика",
+photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2823/04/kucher nataliya viktorivna-683x1024.jpg",
+url: "https://lyceum.ztu.edu.ua/team/kucher-n-v/",
+}
+]
+// Отримання елементу з ідентифікатором Items
+let itemsDiv = document.getElementById("items");
+//Перевірка існування знайденого блоку
+if (itemsDiv) {
+//Створення блоків по кількості елементів масиву
+itemsArray
+//Сортуванен за прізвищем
+//.sort((a, b) a.lastNome.localeCompare(b.lastName))
+    .forEach((item, index) =>{
+//Виводимо на веб сторінку елемент масиву в блок з класом item
+itemsDiv.innerHTML +=
+    `
+        
+        <div class="item">
+        <h2>Вчитель № ${index +1} з ${itemsArray.length}</h2>
+        <p>${item.firstName} ${item.lastName}</p>
+        <p>Предмет: ${item.subject}</p>
+        <p><img src= "${item.photo}" class = "item-image"></p>
+        
         </div>
+
+
+    `
     })
+}
+else{
+
 }
